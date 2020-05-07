9c948ec6eebecfa2a8d93c2235221b7335f96a20
Md Fahad <md.fahad742@gmail.com>
2020-05-07 13:25:53 +0530

    updated

--- Q7_Cousins_In_Binary_Tree.cpp
+++ Q7_Cousins_In_Binary_Tree.cpp
@@ -13,17 +13,17 @@ class Solution {
 public:
     int ans=-1,p=-1;
     
-    void depth(TreeNode* root,int x,int d,int pa){
+    void dfs(TreeNode* root,int x,int d,int pa){
         if(!root)return;
         if(root->val==x){ans=d;p=pa;return;}
-        depth(root->left,x,d+1,root->val);
-        depth(root->right,x,d+1,root->val);
+        dfs(root->left,x,d+1,root->val);
+        dfs(root->right,x,d+1,root->val);
     }
     
     bool isCousins(TreeNode* root, int x, int y) {
-        depth(root,x,0,-1);
+        dfs(root,x,0,-1);
         int op1=ans,pa1=p;
-        depth(root,y,0,-1);
+        dfs(root,y,0,-1);
         int op2=ans,pa2=p;
         return op1==op2 and pa1 != pa2;
     }
