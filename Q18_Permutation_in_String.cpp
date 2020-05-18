6f7ff2372f713c6128a850cd8076c4208ea64d6a
Md Fahad <md.fahad742@gmail.com>
2020-05-18 22:57:57 +0530

    initial commit.

---
+++ Q18_Permutation_in_String.cpp
@@ -0,0 +1,43 @@
+// O(N) - Sliding Window Technique (Same as previous one).
+#define ff first
+#define ss second
+class Solution {
+public:
+    bool checkInclusion(string p, string s) {
+        if(s.size()<p.size()) return 0;
+        
+        map<char,int> mp1;
+        map<char,int> mp2;
+        for(auto ch:p)mp1[ch]++;
+        
+        
+        for(int i=0;i<p.size();i++)mp2[s[i]]++;
+        
+        
+        
+        vector<int> v;
+        bool f1=1;
+        for(auto it:mp1){
+            if(mp2[it.ff]!=it.ss){
+                f1=0;
+                break;
+            }
+        }
+        if(f1)v.push_back(0);
+        
+        for(int i=p.size();i<s.size();i++){
+            mp2[s[i-p.size()]]--;
+            mp2[s[i]]++;
+            bool f=1;
+            for(auto it:mp1){
+                if(mp2[it.ff]!=it.ss){
+                    f=0;
+                    break;
+                }
+            }
+            if(f)v.push_back(i-p.size()+1);
+        }
+        
+        return v.size()>0;
+    }
+};
